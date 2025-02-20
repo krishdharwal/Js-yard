133a8b625d6e8b3c0b8a2be6a9aea70bbcaab142
krishdharwal <darkpirate47171@gmail.com>
2025-02-20 10:34:40 +0530

    first file

---
+++ Untitled-1.js
@@ -0,0 +1,19 @@
+// console.log("fdjaoisfh")
+const num = 123
+let arr = [21,213,123,23,3]
+// Array.isArray(arr)
+// console.log(arr)
+
+let fun = function(){
+    return 123;
+}
+
+console.log(fun())
+
+const obj = {
+    name : "krish",
+    rollno  : 123,
+    steps : 1111
+}
+
+obj.name;
