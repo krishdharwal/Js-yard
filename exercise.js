146ebd48c65f4a9e215ed1418423224485f35cb3
krishdharwal <darkpirate47171@gmail.com>
2025-05-04 19:05:06 +0530

    Promise Questions stared

--- exercise.js
+++ exercise.js
@@ -18,7 +18,23 @@
 
 // obj.name;
 
-const arr = [1,2,3,4,5];
-if(arr.find(2)){
-    console.log("ok")
-}
+
+// making of an array of array
+const arr1 = [1,2];
+const arr2 = [4,2];
+const arr3 = [2,2];
+
+// if(arr.includes(2)){
+//     console.log(" ok")
+// }
+
+const arr = [arr1,arr2];
+arr.push(arr3)
+// console.log(arr);
+arr.forEach((ele) => {
+    console.log(ele);
+})
+
+if(arr.includes([2,2])){
+    console.log(" ok")
+}else console.log()
